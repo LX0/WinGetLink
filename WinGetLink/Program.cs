cc96418f028f6cb5c5b451031d14ce45f1408608
pmb134 <tlyy1159@qq.com>
2018-12-06 19:42:17 +0800

    Update Program.cs

--- WinGetLink/Program.cs
+++ WinGetLink/Program.cs
@@ -15,7 +15,7 @@ namespace WinGetLink
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form1() { StartPosition=FormStartPosition.CenterScreen});
         }
     }
 }
