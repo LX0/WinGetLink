27f4e2d254fb6525c2dbc8552e59ef3c0b5bc6ab
pmb134 <tlyy1159@qq.com>
2018-12-06 19:37:15 +0800

    1

--- WinGetLink/Form1.Designer.cs
+++ WinGetLink/Form1.Designer.cs
@@ -28,12 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtold = new System.Windows.Forms.TextBox();
+            this.txtnew = new System.Windows.Forms.TextBox();
+            this.buttonChange = new System.Windows.Forms.Button();
+            this.btnfile = new System.Windows.Forms.Button();
+            this.cbpwd = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtsp = new System.Windows.Forms.TextBox();
+            this.labtip = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "将文本复制到待转换区";
+            // 
+            // txtold
+            // 
+            this.txtold.Location = new System.Drawing.Point(12, 48);
+            this.txtold.Multiline = true;
+            this.txtold.Name = "txtold";
+            this.txtold.Size = new System.Drawing.Size(732, 330);
+            this.txtold.TabIndex = 1;
+            // 
+            // txtnew
+            // 
+            this.txtnew.Location = new System.Drawing.Point(12, 433);
+            this.txtnew.Multiline = true;
+            this.txtnew.Name = "txtnew";
+            this.txtnew.Size = new System.Drawing.Size(732, 165);
+            this.txtnew.TabIndex = 1;
+            // 
+            // buttonChange
+            // 
+            this.buttonChange.Location = new System.Drawing.Point(307, 385);
+            this.buttonChange.Name = "buttonChange";
+            this.buttonChange.Size = new System.Drawing.Size(98, 42);
+            this.buttonChange.TabIndex = 2;
+            this.buttonChange.Text = "获取链接";
+            this.buttonChange.UseVisualStyleBackColor = true;
+            // 
+            // btnfile
+            // 
+            this.btnfile.Location = new System.Drawing.Point(557, 385);
+            this.btnfile.Name = "btnfile";
+            this.btnfile.Size = new System.Drawing.Size(98, 42);
+            this.btnfile.TabIndex = 2;
+            this.btnfile.Text = "生成文件";
+            this.btnfile.UseVisualStyleBackColor = true;
+            // 
+            // cbpwd
+            // 
+            this.cbpwd.AutoSize = true;
+            this.cbpwd.Checked = true;
+            this.cbpwd.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbpwd.Location = new System.Drawing.Point(207, 23);
+            this.cbpwd.Name = "cbpwd";
+            this.cbpwd.Size = new System.Drawing.Size(240, 16);
+            this.cbpwd.TabIndex = 3;
+            this.cbpwd.Text = "提取密码（仅支持链接和密码在同一行）";
+            this.cbpwd.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(492, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "分隔符";
+            // 
+            // txtsp
+            // 
+            this.txtsp.Location = new System.Drawing.Point(555, 20);
+            this.txtsp.Name = "txtsp";
+            this.txtsp.Size = new System.Drawing.Size(100, 21);
+            this.txtsp.TabIndex = 5;
+            this.txtsp.Text = "----";
+            // 
+            // labtip
+            // 
+            this.labtip.AutoSize = true;
+            this.labtip.Location = new System.Drawing.Point(34, 611);
+            this.labtip.Name = "labtip";
+            this.labtip.Size = new System.Drawing.Size(11, 12);
+            this.labtip.TabIndex = 0;
+            this.labtip.Text = ".";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(756, 643);
+            this.Controls.Add(this.txtsp);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbpwd);
+            this.Controls.Add(this.btnfile);
+            this.Controls.Add(this.buttonChange);
+            this.Controls.Add(this.txtnew);
+            this.Controls.Add(this.txtold);
+            this.Controls.Add(this.labtip);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "批量获取网盘链接";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtold;
+        private System.Windows.Forms.TextBox txtnew;
+        private System.Windows.Forms.Button buttonChange;
+        private System.Windows.Forms.Button btnfile;
+        private System.Windows.Forms.CheckBox cbpwd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtsp;
+        private System.Windows.Forms.Label labtip;
     }
 }
 
--- WinGetLink/Form1.cs
+++ WinGetLink/Form1.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
 
 namespace WinGetLink
 {
@@ -14,6 +16,89 @@ namespace WinGetLink
         public Form1()
         {
             InitializeComponent();
+            txtold.TextChanged += txtold_TextChanged;
+            btnfile.Click += btnfile_Click;
+            buttonChange.Click += txtold_TextChanged;
+        }
+
+        void btnfile_Click(object sender, EventArgs e)
+        {
+            var nw = txtnew.Text.Trim();
+            if (string.IsNullOrEmpty(nw))
+            {
+                MessageBox.Show(this, "内容为空,无法保存!");
+                return;
+            }
+
+            var savepath = Application.StartupPath + "\\tmp_" + DateTime.Now.ToString("yyyy_MM_dd_mm_HH_ss") + ".txt";
+            File.WriteAllText(savepath, nw, Encoding.UTF8);
+            WriteTip("保存成功," + savepath);
+        }
+        private readonly string KeyWord = "https:";
+        void txtold_TextChanged(object sender, EventArgs e)
+        {
+            WriteTip("");
+            txtnew.Text = "";
+            var context = txtold.Text;
+
+
+            if (string.IsNullOrEmpty(context)) return;
+
+            var sps = context.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = sps.Length - 1; i >= 0; i--)
+            {
+                if (!sps[i].Contains(KeyWord))
+                    sps[i].Remove(i);
+            }
+            var ispwd = cbpwd.Checked;
+            var spStr = txtsp.Text.Trim();
+
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            foreach (var item in sps)
+            {
+                var rg = new Regex(@"(?=https).*?(?=\s)");
+                var mc = rg.Matches(item);
+                if (mc.Count <= 0) continue;
+                foreach (Match im in mc)
+                {
+                    var url = im.Value;
+                    if (ispwd)
+                    {
+                        //获取密码，密码一般在url的后面
+                        string pwdStr = GetPWD(item, url);
+                        dict.Add(url, pwdStr);
+
+                    }
+                    else
+                        dict.Add(url, "");
+                }
+            }
+            var strlast = new StringBuilder();
+            foreach (var item in dict)
+            {
+                if (ispwd)
+                    strlast.AppendFormat("{0}{1}{2}", item.Key, spStr, item.Value);
+                else
+                    strlast.AppendFormat("{0}", item.Key);
+                strlast.AppendLine();
+            }
+
+            txtnew.Text = strlast.ToString();
+
+        }
+
+        private string GetPWD(string item, string url)
+        {
+            var lastLen = item.IndexOf(url) + url.Length;
+            var laststr = item.Substring(lastLen);
+            var rg = new Regex(@"\w{4}");
+            var mc = rg.Match(laststr);
+            if (!mc.Success) return "";
+            return mc.Value;
+        }
+        private void WriteTip(string msg)
+        {
+            labtip.Text = msg;
         }
     }
 }
